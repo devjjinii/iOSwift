5670b7f8c015a2a0f913e4df6f609c833dc7f1ed
devjjinii <devjjinii@naver.com>
2021-02-13 16:43:55 +0900

    init

---
+++ I Am rich/I Am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am rich
+//
+//  Created by dev jin on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyTodo/MyTodo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyTodo
+//
+//  Created by dev jin on 2021/01/30.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
