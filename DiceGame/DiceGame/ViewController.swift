8523f09c2fee4d6a107745321fed37ac62efe95c
devjjinii <devjjinii@naver.com>
2021-02-27 19:08:10 +0900

    feat: add click event

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        diceImageViewOne.image = #imageLiteral(resourceName: "2")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "6")
+    
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button Click")
+        
+        diceImageViewOne.image = #imageLiteral(resourceName: "3")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "1")
+    }
+    
 }
 
