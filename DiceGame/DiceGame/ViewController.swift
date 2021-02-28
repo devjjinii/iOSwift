f05b3e67035482bd0175e3691953e46214778811
devjjinii <devjjinii@naver.com>
2021-02-28 23:36:03 +0900

    array로 수정

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -10,22 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rigthDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-        diceImageViewOne.image = #imageLiteral(resourceName: "2")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "6")
-    
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button Click")
+//        print("Button Click")
+//        diceImageViewOne.image = #imageLiteral(resourceName: "3")
+//        diceImageViewTwo.image = #imageLiteral(resourceName: "1")
         
-        diceImageViewOne.image = #imageLiteral(resourceName: "3")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "1")
+        diceImageViewOne.image = [ #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ][leftDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
+        
+        diceImageViewTwo.image = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6") ][rigthDiceNumber]
+        rigthDiceNumber = rigthDiceNumber - 1
     }
     
 }
