9d16f92c782d90e75db1a330251e2ea1f910c580
devjjinii <devjjinii@naver.com>
2021-04-12 19:49:32 +0900

    메모앱 프로젝트 추가

---
+++ jjiniiMemo/jjiniiMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jjiniiMemo
+//
+//  Created by dev jin on 2021/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
