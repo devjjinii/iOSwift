27e54b6419291334282762f296a3a31d8347b13d
devjjinii <devjjinii@naver.com>
2021-04-13 10:38:08 +0900

    메모 앱 Memo Class 추가

---
+++ jjiniiMemo/jjiniiMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  jjiniiMemo
+//
+//  Created by dev jin on 2021/04/13.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "찌니의 산책일지"),
+        Memo(content: "Dummy Data2 👩🏻‍💻😎")
+    ]
+}
