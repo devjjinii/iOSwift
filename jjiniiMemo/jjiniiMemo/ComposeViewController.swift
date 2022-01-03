dd4e6a3579f9bb0bfe5ae417e52009198c592f40
devjjinii <devjjinii@naver.com>
2022-01-03 16:07:46 +0900

    메모 앱 상세수정기능 추가

--- jjiniiMemo/jjiniiMemo/ComposeViewController.swift
+++ jjiniiMemo/jjiniiMemo/ComposeViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    var editTarget: Memo?
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -26,10 +27,16 @@ class ComposeViewController: UIViewController {
         
 //        let newMemo = Memo(content: memo)
 //        Memo.dummyMemoList.append(newMemo)
-        DataManager.shared.addNewMemo(memo)
         
-        // notification 전달
-        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        if let target = editTarget {
+            target.content = memo
+            DataManager.shared.saveContext()
+            NotificationCenter.default.post(name: ComposeViewController.memoDidChange, object: nil)
+        } else {
+            DataManager.shared.addNewMemo(memo)
+            // notification 전달
+            NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        }
         
         // 닫기
         dismiss(animated: true, completion: nil)
@@ -38,8 +45,15 @@ class ComposeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let memo = editTarget {
+            navigationItem.title = "메모 편집"
+            memoTextView.text = memo.content
+        } else {
+            navigationItem.title = "새 메모"
+            memoTextView.text = ""
+        }
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -57,4 +71,6 @@ class ComposeViewController: UIViewController {
 
 extension ComposeViewController {
     static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
+    
+    static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
 }
